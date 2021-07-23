1875bb3fd8d021f057bdba490aa1b6b676f3aade
Tom Walker <tom.n.walker@gmail.com>
2021-07-23 11:37:10 +0200

    Addded molecular fingerprint to metabolite information

--- code_clean/clean_metabolites.R
+++ code_clean/clean_metabolites.R
@@ -36,10 +36,41 @@ clean_metabolites <- function(other_data, distance){
   mtbs <- other_data$om_composition$compounds %>%
     # format to make compound names match peaks column name
     mutate(column_name = make.names(column_name))
+  # get smiles and add to data frame
+  smiles <- cir_query(inchikey, match = "first")
+  smiles[names(smiles) == "VRQGSFPALWCJBE-RQYWWVFRSA-N"] <- "CO[C@H]1[C@@H]([C@H]([C@H](O[C@@H]1C(=O)OC)OC)O)O"
+  smiles[names(smiles) == "OCJFQVCIXIGACT-RBXMUDONSA-N"] <- "CC(=O)O[C@@H]1[C@H](CO[C@@H]([C@H]1O)OC(=O)C)O"
+  smiles[names(smiles) == "JVBICOCAMPYKMA-UIAUGNHASA-N"] <- "CC(=O)O[C@H]1[C@H]2[C@H](O2)[C@H]3CO[C@@H]1O3"
+  smilesDF <- data.frame(
+    inchikey = names(smiles),
+    smiles = smiles
+  )
+  mtbs <- left_join(mtbs, smilesDF)
   
   ## Derive sample-wise chemical information and info theory metrics ----
   chemInfo <- get_atom_counts(relAbun, mtbs)
   infoThry <- apply_info_theory(relAbun)
+  
+  ## Get molecular fingerprints ----
+  # generate smiles vectors
+  smilesVector <- mtbs$smiles
+  names(smilesVector) <- mtbs$name
+  # parse to correct atom format
+  smilesParsed <- parse.smiles(smilesVector, omit.nulls = T)
+  # generate molecular fingerprints
+  fingerprints <- lapply(smilesParsed, get.fingerprint, type = "extended")
+  # build similarities
+  similarities <- fingerprint::fp.sim.matrix(fingerprints, method = "tanimoto")
+  # convert to dissimilarities
+  dissim <- 1 - similarities
+  # add row and column names
+  colnames(dissim) <- rownames(dissim) <- names(smilesParsed)
+  # remove large redundant objects (occasional hang)
+  rm(smilesParsed, fingerprints, similarities)
+  # convert to distance object
+  dissim <- as.dist(dissim)
+  # cluster
+  mtbClust <- hclust(dissim)
   
   ## Collate and return ----
   # create sample ID column
@@ -64,7 +95,7 @@ clean_metabolites <- function(other_data, distance){
            pcoa2 = ordinations$pcoa[, 2])
   # add compound specificity to mtbs dataset
   mtbsOut <- mtbs %>%
-    select(inchikey, column_name, name, C:complexity) %>%
+    select(inchikey, smiles, column_name, name, C:complexity) %>%
     mutate(specificity = infoThry$varSpecificity,
            nmds1loadings = ordinations$nmds$species[, 1],
            nmds2loadings = ordinations$nmds$species[, 2])
@@ -72,6 +103,7 @@ clean_metabolites <- function(other_data, distance){
   out <- list(
     peakAreas = relAbunOut,
     nmds = ordinations$nmds,
+    clustered_compounds = mtbsClust,
     chemistry = chemInfoOut,
     compounds = mtbsOut
   )
